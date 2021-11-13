200eb5e3871ae1d558ad2aef549b587e5c833327
aqua5201314 <aqua5201314@outlook.com>
2021-11-12 21:11:58 -0800

    添加连接池模块

--- connection.cc
+++ connection.cc
@@ -2,54 +2,46 @@
 
 #include <iostream>
 
-// 初始化数据库连接
 Connection::Connection()
 {
-    _conn = mysql_init(nullptr);
+	// 初始化数据库连接
+	_conn = mysql_init(nullptr);
 }
 
-// 释放数据库连接资源
 Connection::~Connection()
 {
-    if (_conn != nullptr)
-    {
-        mysql_close(_conn);
-    }
+	// 释放数据库连接资源
+	if (_conn != nullptr)
+		mysql_close(_conn);
 }
 
-// 连接数据库
-bool Connection::connect(std::string ip,
-                         unsigned short port,
-                         std::string username,
-                         std::string password,
-                         std::string dbname)
+bool Connection::connect(std::string ip, unsigned short port, 
+	std::string username, std::string password, std::string dbname)
 {
-    // 连接数据库
-    MYSQL *p = mysql_real_connect(_conn, ip.c_str(), username.c_str(),
-                                  password.c_str(), dbname.c_str(), port, nullptr, 0);
-    return p != nullptr;
+	// 连接数据库
+	MYSQL *p = mysql_real_connect(_conn, ip.c_str(), username.c_str(),
+		password.c_str(), dbname.c_str(), port, nullptr, 0);
+	return p != nullptr;
 }
 
-// 更新操作 insert/delete/update
 bool Connection::update(std::string sql)
 {
-    if (mysql_query(_conn, sql.c_str()))
-    {
-        // TODO LOG
-        std::cout << "更新失败:" << sql << std::endl;
-        return false;
-    }
-    return true;
+	// 更新操作 insert、delete、update
+	if (mysql_query(_conn, sql.c_str()))
+	{
+		LOG("更新失败:" + sql);
+		return false;
+	}
+	return true;
 }
 
-// 查询操作 select
-MYSQL_RES *Connection::query(std::string sql)
+MYSQL_RES* Connection::query(std::string sql)
 {
-    if (mysql_query(_conn, sql.c_str()))
-    {
-        // TODO LOG
-        std::cout << "查询失败:" << sql << std::endl;
-        return nullptr;
-    }
-    return mysql_use_result(_conn);
+	// 查询操作 select
+	if (mysql_query(_conn, sql.c_str()))
+	{
+		LOG("查询失败:" + sql);
+		return nullptr;
+	}
+	return mysql_use_result(_conn);
 }
--- main.cc
+++ main.cc
@@ -1,14 +1,120 @@
 #include "connection.h"
+#include "connectionpool.h"
 
 #include <iostream>
 
 int main()
 {
     Connection conn;
-    conn.connect("127.0.0.1", 3306, "root", "123456", "connpool");
+    conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
 
-    char sql[1024] = {0};
-    sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
-            "zhang san", 20, "male");
-    conn.update(sql);
+    /*Connection conn;
+	char sql[1024] = { 0 };
+	sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+		"zhang san", 20, "male");
+	conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+	conn.update(sql);*/
+
+    clock_t begin = clock();
+
+    std::thread t1([]()
+              {
+                  //ConnectionPool *cp = ConnectionPool::getConnectionPool();
+                  for (int i = 0; i < 2500; ++i)
+                  {
+                      /*char sql[1024] = { 0 };
+			sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+				"zhang san", 20, "male");
+			shared_ptr<Connection> sp = cp->getConnection();
+			sp->update(sql);*/
+                      Connection conn;
+                      char sql[1024] = {0};
+                      sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+                              "zhang san", 20, "male");
+                      conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+                      conn.update(sql);
+                  }
+              });
+    std::thread t2([]()
+              {
+                  //ConnectionPool *cp = ConnectionPool::getConnectionPool();
+                  for (int i = 0; i < 2500; ++i)
+                  {
+                      /*char sql[1024] = { 0 };
+			sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+				"zhang san", 20, "male");
+			shared_ptr<Connection> sp = cp->getConnection();
+			sp->update(sql);*/
+                      Connection conn;
+                      char sql[1024] = {0};
+                      sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+                              "zhang san", 20, "male");
+                      conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+                      conn.update(sql);
+                  }
+              });
+    std::thread t3([]()
+              {
+                  //ConnectionPool *cp = ConnectionPool::getConnectionPool();
+                  for (int i = 0; i < 2500; ++i)
+                  {
+                      /*char sql[1024] = { 0 };
+			sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+				"zhang san", 20, "male");
+			shared_ptr<Connection> sp = cp->getConnection();
+			sp->update(sql);*/
+                      Connection conn;
+                      char sql[1024] = {0};
+                      sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+                              "zhang san", 20, "male");
+                      conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+                      conn.update(sql);
+                  }
+              });
+    std::thread t4([]()
+              {
+                  //ConnectionPool *cp = ConnectionPool::getConnectionPool();
+                  for (int i = 0; i < 2500; ++i)
+                  {
+                      /*char sql[1024] = { 0 };
+			sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+				"zhang san", 20, "male");
+			shared_ptr<Connection> sp = cp->getConnection();
+			sp->update(sql);*/
+                      Connection conn;
+                      char sql[1024] = {0};
+                      sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+                              "zhang san", 20, "male");
+                      conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+                      conn.update(sql);
+                  }
+              });
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    clock_t end = clock();
+    std::cout << (end - begin) << "ms" << std::endl;
+
+#if 0
+	for (int i = 0; i < 10000; ++i)
+	{
+		Connection conn;
+		char sql[1024] = { 0 };
+		sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+			"zhang san", 20, "male");
+		conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+		conn.update(sql);
+
+		/*shared_ptr<Connection> sp = cp->getConnection();
+		char sql[1024] = { 0 };
+		sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+			"zhang san", 20, "male");
+		sp->update(sql);*/
+	}
+#endif
+
+    return 0;
 }
