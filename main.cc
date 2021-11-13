56f3d23b8317cc7b56e8837e705ca43a428773c3
aqua5201314 <aqua5201314@outlook.com>
2021-11-12 20:49:27 -0800

    测试mysql修改

--- main.cc
+++ main.cc
@@ -2,9 +2,13 @@
 
 #include <iostream>
 
-int main(){
+int main()
+{
     Connection conn;
-    if(conn.connect("127.0.0.1",3306,"root","123456","connpool")){
-        std::cout<<"connection success!"<<std::endl;
-    }
+    conn.connect("127.0.0.1", 3306, "root", "123456", "connpool");
+
+    char sql[1024] = {0};
+    sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
+            "zhang san", 20, "male");
+    conn.update(sql);
 }
